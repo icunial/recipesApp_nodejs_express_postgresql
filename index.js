988a27e39fd38294c68f34957f90015e29bc7c1c
Ignacio Cunial <igsecu@hotmail.com>
2023-08-10 16:42:03 -0300

    Created new Pool -> used process.env variables

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const express = require("express");
 const app = express();
-const pg = require("pg");
 
-// DB Connect String
-const pg_connect = "postgres://postgres@localhost/recipes_db";
+const Pool = require("pg").Pool;
+
+require("dotenv").config();
+
+console.log(process.env);
+
+// Init Pool
+const pool = new Pool({
+  user: process.env.USERNAME_DB,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.PORT_DB,
+});
 
 // Body Parser Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.send("Recipes App");
+  pool.query("SELECT * FROM recipes", (err, results) => {
+    if (err) {
+      throw err;
+    }
+    return res.status(200).json(results.rows);
+  });
 });
 
 const PORT = process.env.PORT || 5000;
