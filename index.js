1cb412ca4afd62241fb60d95f3078bdd151606fa
Ignacio Cunial <igsecu@hotmail.com>
2023-08-10 15:53:54 -0300

    Created GET route demo -> index.js / Created routers folder

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const app = express();
+const pg = require("pg");
 
-// Body Parser
+// DB Connect String
+const pg_connect = "postgres://postgres@localhost/recipes_db";
+
+// Body Parser Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("Recipes App");
+});
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server listen on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`);
 });
