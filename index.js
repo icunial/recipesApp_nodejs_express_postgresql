f6b488b0e846fba5b140fa58267819b079464b9e
Ignacio Cunial <igsecu@hotmail.com>
2023-08-10 15:45:54 -0300

    Initiated express -> index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const express = require("express");
+const app = express();
 
+// Body Parser
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server listen on port ${PORT}`);
+});
