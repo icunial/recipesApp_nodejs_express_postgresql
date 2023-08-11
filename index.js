9f12703861e415c69906283b391a2c794dd7b74e
Ignacio Cunial <igsecu@hotmail.com>
2023-08-11 10:17:48 -0300

    Modularized recipesRouter -> /routers/recipes.js

--- index.js
+++ index.js
@@ -1,89 +1,16 @@
 const express = require("express");
 const app = express();
 
-const Pool = require("pg").Pool;
-
 require("dotenv").config();
 
-console.log(process.env);
-
-// Init Pool
-const pool = new Pool({
-  user: process.env.USERNAME_DB,
-  host: process.env.HOST,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
-  port: process.env.PORT_DB,
-});
+const router = require("./routers/recipes");
 
 // Body Parser Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Get all recipes
-app.get("/", (req, res) => {
-  pool.query("SELECT * FROM recipes", (err, results) => {
-    if (err) {
-      throw err;
-    }
-    return res.status(200).json(results.rows);
-  });
-});
-
-// Get Recipe by ID
-app.get("/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-
-  pool.query(
-    "SELECT * FROM recipes WHERE id = $1",
-    [req.params.id],
-    (err, results) => {
-      if (err) {
-        throw err;
-      }
-      return res.status(200).json(results.rows);
-    }
-  );
-});
-
-// Create new Recipe
-app.post("/", (req, res) => {
-  const { name, ingredients, directions } = req.body;
-
-  pool.query(
-    "INSERT INTO recipes (name, ingredients, directions) VALUES ($1, $2, $3)",
-    [name, ingredients, directions],
-    (err, results) => {
-      if (err) throw err;
-      return res.status(201).json(results.rows);
-    }
-  );
-});
-
-// Delete recipe by id
-app.delete("/:id", (req, res) => {
-  const id = req.params.id;
-
-  pool.query("DELETE FROM recipes WHERE id = $1", [id], (err, results) => {
-    if (err) throw err;
-    return res.status(200).json(results);
-  });
-});
-
-// Update recipe by id
-app.put("/:id", (req, res) => {
-  const id = req.params.id;
-  const recipeInfo = req.body;
-
-  pool.query(
-    "UPDATE recipes SET name = $1, ingredients = $2, directions = $3 WHERE id = $4",
-    [recipeInfo.name, recipeInfo.ingredients, recipeInfo.directions, id],
-    (err, results) => {
-      if (err) throw err;
-      return res.status(200).json(results);
-    }
-  );
-});
+// Routers
+app.use("/api/recipes", router);
 
 const PORT = process.env.PORT || 5000;
 
---
+++ routers/recipes.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const router = express.Router();
+
+const Pool = require("pg").Pool;
+
+// Init Pool
+const pool = new Pool({
+  user: process.env.USERNAME_DB,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.PORT_DB,
+});
+
+// Get all recipes
+router.get("/", (req, res) => {
+  pool.query("SELECT * FROM recipes", (err, results) => {
+    if (err) {
+      throw err;
+    }
+    return res.status(200).json(results.rows);
+  });
+});
+
+// Get Recipe by ID
+router.get("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  pool.query(
+    "SELECT * FROM recipes WHERE id = $1",
+    [req.params.id],
+    (err, results) => {
+      if (err) {
+        throw err;
+      }
+      return res.status(200).json(results.rows);
+    }
+  );
+});
+
+// Create new Recipe
+router.post("/", (req, res) => {
+  const { name, ingredients, directions } = req.body;
+
+  pool.query(
+    "INSERT INTO recipes (name, ingredients, directions) VALUES ($1, $2, $3)",
+    [name, ingredients, directions],
+    (err, results) => {
+      if (err) throw err;
+      return res.status(201).json(results.rows);
+    }
+  );
+});
+
+// Delete recipe by id
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+
+  pool.query("DELETE FROM recipes WHERE id = $1", [id], (err, results) => {
+    if (err) throw err;
+    return res.status(200).json(results);
+  });
+});
+
+// Update recipe by id
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  const recipeInfo = req.body;
+
+  pool.query(
+    "UPDATE recipes SET name = $1, ingredients = $2, directions = $3 WHERE id = $4",
+    [recipeInfo.name, recipeInfo.ingredients, recipeInfo.directions, id],
+    (err, results) => {
+      if (err) throw err;
+      return res.status(200).json(results);
+    }
+  );
+});
+
+module.exports = router;
